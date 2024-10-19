2ea935d1e423009b10538336718975669eb18a6d
KostiantynPolishko2 <polxs_wp31@student.itstep.org>
2024-10-19 18:01:26 +0300

    3 created class Marker

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 print('Hello world!')
+
+a = 2
+b = 'hello'
+c = True
+d = 2.2
+
+
+class Marker:
+    def __init__(self, color='black'):
+        self.color = color
+        self.weight = 1.22
+        self.price = 3
+        self.isPresent = True
+
+    def getInfo(self):
+        print(f'color:{self.color}-price:{self.price}UAH')
+
+
+blackMarker = Marker()
+# print(blackMarker.color)
+blackMarker.getInfo()
+
+redMarker = Marker('red')
+# print(redMarker.color)
+redMarker.getInfo()
